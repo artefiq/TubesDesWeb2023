7d249d6d6a241394df978fd0a2eea42688f59a48
Ahmad Taufiq H <73924063+artefiq@users.noreply.github.com>
2024-08-28 15:28:31 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/application/ci/tubes_dpw/';
+$config['base_url'] = 'http://localhost/TubesDesWeb2023';
 
 /*
 |--------------------------------------------------------------------------
